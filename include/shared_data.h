e55b441e68ac7e8f98dc782e321e5b23645ae971
Leo Cozzens <leo@narthexnetworks.com>
2023-08-16 00:55:22 -0700

    Compacted board typedef

--- include/shared_data.h
+++ include/shared_data.h
@@ -2,10 +2,9 @@
 #define SHARED_DATA_H
 
 typedef struct _Dimension Dimension;
-typedef struct _Board Board;
 
-struct _Board {
+typedef struct _Board {
     Dimension *windowDimension;
-};
+} Board;
 
 #endif
